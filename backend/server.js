86485210376932c5fc5e9747c40de41613ab4f32
Justin Scorzafava <jkscorzafava@gmail.com>
2022-06-20 16:08:54 -0600

    added routes for workouts

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const workoutRoutes = require("./routes/workouts");
 
 const app = express();
 
@@ -11,9 +12,7 @@ app.use((req, res, next) => {
 });
 
 // routes
-app.get("/", (req, res) => {
-	res.json({ mssg: "Welcome to the app" });
-});
+app.use("/api/workouts", workoutRoutes);
 
 app.listen(process.env.PORT, () => {
 	console.log("listening on port 4000");
