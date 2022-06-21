de0d0c748fc199b4abfd15d52c7ae0cfe28197e9
Justin Scorzafava <jkscorzafava@gmail.com>
2022-06-21 11:25:56 -0600

    finished contorller functions for deleteWorkout and updateWorkout

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,5 +1,6 @@
 const Workout = require("../models/workoutModel");
 const mongoose = require("mongoose");
+const { response } = require("express");
 
 // get all workouts
 const getWorkouts = async (req, res) => {
@@ -34,11 +35,48 @@ const createWorkout = async (req, res) => {
 	}
 };
 // delete a workout
+const deleteWorkout = async (req, res) => {
+	const { id } = req.params;
+
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(404).json({ error: "No such workout" });
+	}
+
+	const workout = await Workout.findOneAndDelete({ _id: id });
+
+	if (!workout) {
+		return res.status(404).json({ error: "No such workout" });
+	}
+
+	res.status(200).json(workout);
+};
 
 // update a workout
+const updateWorkout = async (req, res) => {
+	const { id } = req.params;
+
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(404).json({ error: "No such workout" });
+	}
+
+	const workout = await Workout.findOneAndUpdate(
+		{ _id: id },
+		{
+			...req.body,
+		}
+	);
+
+	if (!workout) {
+		return res.status(400).json({ error: "No such workout" });
+	}
+
+	return res.status(200).json(workout);
+};
 
 module.exports = {
 	getWorkout,
 	getWorkouts,
 	createWorkout,
+	deleteWorkout,
+	updateWorkout,
 };
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -3,6 +3,8 @@ const {
 	createWorkout,
 	getWorkout,
 	getWorkouts,
+	deleteWorkout,
+	updateWorkout,
 } = require("../controllers/workoutController");
 
 const router = express.Router();
@@ -17,13 +19,9 @@ router.get("/:id", getWorkout);
 router.post("/", createWorkout);
 
 // delete a workout
-router.delete("/:id", (req, res) => {
-	res.json({ mssg: "DELETE a new workout" });
-});
+router.delete("/:id", deleteWorkout);
 
 // update a workout
-router.patch("/:id", (req, res) => {
-	res.json({ mssg: "UPDATE a new workout" });
-});
+router.patch("/:id", updateWorkout);
 
 module.exports = router;
