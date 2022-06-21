892ac23bb17581f1c3c0b9fa9acea2f460d71c76
Justin Scorzafava <jkscorzafava@gmail.com>
2022-06-21 14:42:16 -0600

    updated img logo and styles

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,7 +3,7 @@ import logo from "../assets/Reps-Logger.svg";
 const Navbar = () => {
 	return (
 		<header>
-			<div className='flex items-center justify-between max-w-full bg-gray-300 px-16 py-10 mx-auto'>
+			<div className='flex items-center justify-between max-w-full bg-slate-900 bg-opacity-90 px-16 py-10 mx-auto'>
 				<Link to='/'>
 					<img src={logo} alt='' />
 				</Link>
