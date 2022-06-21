f902d395f98e38016d6527d9f4f8b8c9d63f154a
Justin Scorzafava <jkscorzafava@gmail.com>
2022-06-21 14:46:10 -0600

    changed hover on logo

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,7 +5,11 @@ const Navbar = () => {
 		<header>
 			<div className='flex items-center justify-between max-w-full bg-slate-900 bg-opacity-90 px-16 py-10 mx-auto'>
 				<Link to='/'>
-					<img src={logo} alt='' />
+					<img
+						src={logo}
+						alt=''
+						className='hover:opacity-80 duration-200 transition'
+					/>
 				</Link>
 			</div>
 		</header>
