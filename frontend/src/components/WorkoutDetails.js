9b767c214a276685a21359a9a43f6fc2324e66b1
Justin Scorzafava <jkscorzafava@gmail.com>
2022-06-22 11:12:27 -0600

    added DELETE_WORKOUT dispatch action to reducer and delete icon

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,4 +1,20 @@
+import { MdDeleteForever } from "react-icons/md";
+import { useWorkoutsContext } from "../hooks/useWorkoutContext";
+
 const WorkoutDetails = ({ workout }) => {
+	const { dispatch } = useWorkoutsContext();
+
+	const handleClick = async () => {
+		const response = await fetch("/api/workouts/" + workout._id, {
+			method: "DELETE",
+		});
+		const json = await response.json();
+
+		if (response.ok) {
+			dispatch({ type: "DELETE_WORKOUT", payload: json });
+		}
+	};
+
 	return (
 		<div className='flex flex-col  items-center space-y-1 bg-primary rounded-md my-10 mx-auto p-8 relative shadow-md md:items-start'>
 			<h4 className=' text-xl text-white mb-3 font-bold'>
@@ -13,6 +29,12 @@ const WorkoutDetails = ({ workout }) => {
 				{workout.reps}
 			</p>
 			<p className='text-md text-slate-800'>{workout.createdAt}</p>
+			<span>
+				<MdDeleteForever
+					onClick={handleClick}
+					className='text-3xl text-red-700 cursor-pointer'
+				/>
+			</span>
 		</div>
 	);
 };
