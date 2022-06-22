aacb6b977d64b2845b61c3bdac5748a0dd8cabea
Justin Scorzafava <jkscorzafava@gmail.com>
2022-06-22 10:02:47 -0600

    added useWorkoutsContext to provide context to different components and allow to create workouts and set workouts as dispatch types

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,7 +3,7 @@ import logo from "../assets/Reps-Logger.svg";
 const Navbar = () => {
 	return (
 		<header>
-			<div className='flex items-center justify-between max-w-full bg-slate-900 bg-opacity-90 px-20 py-6 mx-auto'>
+			<div className='flex items-center justify-between max-w-full bg-cyan-800 bg-opacity-90 px-20 py-6 mx-auto'>
 				<Link to='/'>
 					<img
 						src={logo}
--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutContext";
 
 const WorkoutForm = () => {
+	const { dispatch } = useWorkoutsContext();
+
 	const [title, setTitle] = useState("");
 	const [load, setLoad] = useState("");
 	const [reps, setReps] = useState("");
@@ -29,34 +32,37 @@ const WorkoutForm = () => {
 			setReps("");
 			setError(null);
 			console.log(json);
+			dispatch({ type: "CREATE_WORKOUT", payload: json });
 		}
 	};
 
 	return (
 		<form onSubmit={handleSubmit}>
-			<h3 className='text-xl my-5 text-slate-900'>Add a New Workout</h3>
+			<h3 className='text-xl my-5 text-slate-900 font-bold'>
+				Add a New Workout
+			</h3>
 			<label className='block'>Exercise Title:</label>
 			<input
 				type='text'
 				onChange={(e) => setTitle(e.target.value)}
 				value={title}
-				className='block p-3 mt-5 mb-5 w-full border rounded-md '
+				className='block p-2 mt-5 mb-5 w-full border rounded-md '
 			/>
 			<label className='block'>Weight (in lbs):</label>
 			<input
 				type='number'
 				onChange={(e) => setLoad(e.target.value)}
 				value={load}
-				className='block p-3 mt-2 mb-5 w-full border rounded-md'
+				className='block p-2 mt-2 mb-5 w-full border rounded-md'
 			/>
 			<label className='block'>Reps:</label>
 			<input
 				type='number'
 				onChange={(e) => setReps(e.target.value)}
 				value={reps}
-				className='block p-3 mt-2 mb-5 w-full border rounded-md'
+				className='block p-2 mt-2 mb-5 w-full border rounded-md'
 			/>
-			<button className='bg-primary py-2 px-5 rounded-md border border-transparent cursor-pointer transition duration-300 hover:bg-secondary hover:text-slate-900 uppercase'>
+			<button className='bg-cyan-700 text-white py-2 px-5 rounded-md border border-transparent cursor-pointer transition duration-300 hover:bg-primary hover:text-slate-900 uppercase'>
 				Add Workout
 			</button>
 			{error && (
