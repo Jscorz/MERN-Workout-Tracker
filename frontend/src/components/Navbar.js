b7904bde0d5d2eca16db77921865685aff738fff
Jscorz <jkscorzafava@gmail.com>
2022-09-06 14:56:46 -0600

    added logout button to navbar and imported useLogout()

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,14 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 import logo from "../assets/Reps-Logger.svg";
+
 const Navbar = () => {
+	const { logout } = useLogout();
+
+	const handleClick = () => {
+		logout();
+	};
+
 	return (
 		<header>
 			<div className='flex items-center justify-between max-w-full bg-primary bg-opacity-90 px-20 py-6 mx-auto'>
@@ -11,7 +19,16 @@ const Navbar = () => {
 						className='hover:opacity-80 duration-200 transition'
 					/>
 				</Link>
-				<nav>
+				<nav className='flex items-center'>
+					<div>
+						<button
+							className='text-white font-bold mr-4 p-3 border-2 border-white rounded-lg '
+							onClick={handleClick}
+						>
+							Log out
+						</button>
+					</div>
+
 					<div>
 						<Link
 							to='/login'
