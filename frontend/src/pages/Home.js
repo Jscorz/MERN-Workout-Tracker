43f6f64e36afbd21b234c455b1f616ad3f5c327d
Jscorz <jkscorzafava@gmail.com>
2022-09-07 17:52:53 -0600

    added if block to check if user and only fetchWorkouts() if user is logged in, added headers: Authorization: Bearer user.token in post request to only request workouts with user token

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,24 +1,32 @@
 import { useEffect } from "react";
 import { useWorkoutsContext } from "../hooks/useWorkoutContext";
+import { useAuthContext } from "../hooks/useWorkoutContext";
 
 import WorkoutDetails from "../components/WorkoutDetails";
 import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
 	const { workouts, dispatch } = useWorkoutsContext();
+	const { user } = useAuthContext();
 
 	useEffect(() => {
 		const fetchWorkouts = async () => {
-			const response = await fetch("/api/workouts");
+			const response = await fetch("/api/workouts", {
+				headers: {
+					Authorization: `Bearer ${user.token}`,
+				},
+			});
+
 			const json = await response.json();
 
 			if (response.ok) {
 				dispatch({ type: "SET_WORKOUTS", payload: json });
 			}
 		};
-
-		fetchWorkouts();
-	}, [dispatch]);
+		if (user) {
+			fetchWorkouts();
+		}
+	}, [dispatch, user]);
 
 	return (
 		<div className='md:grid md:grid-cols-4 md:gap-24'>
