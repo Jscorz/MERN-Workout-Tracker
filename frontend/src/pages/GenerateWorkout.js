50794fae2c3c648cc90980aae69d63776017a59b
Justin Scorzafava <jkscorzafava@gmail.com>
2022-09-28 17:55:59 -0600

    installed crossenv to frontend project to process env variable for api

--- frontend/src/pages/GenerateWorkout.js
+++ frontend/src/pages/GenerateWorkout.js
@@ -5,25 +5,24 @@ import { useAuthContext } from "../hooks/useAuthContext";
 const GenerateWorkout = () => {
 	const { workouts, dispatch } = useWorkoutsContext();
 	const { user } = useAuthContext();
+	const API_KEY = process.env.REACT_APP_API_KEY;
 
 	useEffect(() => {
-		const fetchWorkouts = async () => {
-			const response = await fetch("/api/workouts", {
-				headers: {
-					Authorization: `Bearer ${user.token}`,
-				},
-			});
-
-			const json = await response.json();
-
-			if (response.ok) {
-				dispatch({ type: "SET_WORKOUTS", payload: json });
-			}
+		const options = {
+			method: "GET",
+			headers: {
+				"X-RapidAPI-Key": API_KEY,
+				"X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+			},
 		};
+
+		fetch("https://exercisedb.p.rapidapi.com/exercises", options)
+			.then((response) => response.json())
+			.then((response) => console.log(response))
+			.catch((err) => console.error(err));
 		if (user) {
-			fetchWorkouts();
 		}
-	}, [dispatch, user]);
+	}, []);
 
 	return (
 		<div className='h-full border-2 border-slate-600'>GenerateWorkout</div>
