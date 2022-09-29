15e945daaf5020a4079c5000a0c2b4af101b09fe
Justin Scorzafava <jkscorzafava@gmail.com>
2022-09-28 18:19:10 -0600

    added different fetchs for different exercises, removed unneeded code

--- frontend/src/pages/GenerateWorkout.js
+++ frontend/src/pages/GenerateWorkout.js
@@ -1,11 +1,15 @@
-import React, { useEffect } from "react";
-import { useWorkoutsContext } from "../hooks/useWorkoutContext";
-import { useAuthContext } from "../hooks/useAuthContext";
+import React, { useState, useEffect } from "react";
 
 const GenerateWorkout = () => {
-	const { workouts, dispatch } = useWorkoutsContext();
-	const { user } = useAuthContext();
 	const API_KEY = process.env.REACT_APP_API_KEY;
+
+	const [absExercises, setAbsExercises] = useState();
+	const [pectoralsExercises, setPectoralsExercises] = useState();
+	const [bicepsExercises, setBicepsExercises] = useState();
+	const [tricepsExercises, setTricepsExercises] = useState();
+	const [calvesExercises, setCalvesExercises] = useState();
+	const [quadsExercises, setQuadsExercises] = useState();
+	const [hamstringsExercises, setHamstringsExercises] = useState();
 
 	useEffect(() => {
 		const options = {
@@ -15,17 +19,76 @@ const GenerateWorkout = () => {
 				"X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
 			},
 		};
-
-		fetch("https://exercisedb.p.rapidapi.com/exercises", options)
+		// Fetch Abs Exercises
+		fetch("https://exercisedb.p.rapidapi.com/exercises/target/abs", options)
 			.then((response) => response.json())
-			.then((response) => console.log(response))
+			.then((response) => setAbsExercises(response))
 			.catch((err) => console.error(err));
-		if (user) {
-		}
+		// Fetch Pectorals Exercises
+		fetch(
+			"https://exercisedb.p.rapidapi.com/exercises/target/pectorals",
+			options
+		)
+			.then((response) => response.json())
+			.then((response) => setPectoralsExercises(response))
+			.catch((err) => console.error(err));
+		// Fetch Biceps Exercises
+		fetch(
+			"https://exercisedb.p.rapidapi.com/exercises/target/biceps",
+			options
+		)
+			.then((response) => response.json())
+			.then((response) => setBicepsExercises(response))
+			.catch((err) => console.error(err));
+		// Fetch Triceps Exercises
+		fetch(
+			"https://exercisedb.p.rapidapi.com/exercises/target/triceps",
+			options
+		)
+			.then((response) => response.json())
+			.then((response) => setTricepsExercises(response))
+			.catch((err) => console.error(err));
+		// Fetch Calves Exercises
+		fetch(
+			"https://exercisedb.p.rapidapi.com/exercises/target/calves",
+			options
+		)
+			.then((response) => response.json())
+			.then((response) => setCalvesExercises(response))
+			.catch((err) => console.error(err));
+		// Fetch Quads Exercises
+		fetch(
+			"https://exercisedb.p.rapidapi.com/exercises/target/quads",
+			options
+		)
+			.then((response) => response.json())
+			.then((response) => setQuadsExercises(response))
+			.catch((err) => console.error(err));
+		// Fetch Hamstring Exercises
+		fetch(
+			"https://exercisedb.p.rapidapi.com/exercises/target/hamstrings",
+			options
+		)
+			.then((response) => response.json())
+			.then((response) => setHamstringsExercises(response))
+			.catch((err) => console.error(err));
 	}, []);
 
 	return (
-		<div className='h-full border-2 border-slate-600'>GenerateWorkout</div>
+		<div
+			onClick={console.log(
+				absExercises,
+				pectoralsExercises,
+				bicepsExercises,
+				tricepsExercises,
+				calvesExercises,
+				quadsExercises,
+				hamstringsExercises
+			)}
+			className='h-full border-2 border-slate-600'
+		>
+			GenerateWorkout
+		</div>
 	);
 };
 
