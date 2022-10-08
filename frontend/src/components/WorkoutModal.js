e8d359e3c127629839eddeea5493b2f562729397
Justin Scorzafava <jkscorzafava@gmail.com>
2022-10-07 18:11:29 -0600

    changed title of exercises to {exercise.name} and commented out working example to save api calls

--- frontend/src/components/WorkoutModal.js
+++ frontend/src/components/WorkoutModal.js
@@ -90,6 +90,163 @@ function WorkoutModal({
 		}
 	}, []);
 
+	//     // Working example with dyanmic exercise data pulled from API
+	// 	return (
+	// 		<section className='absolute h-[120%] top-0 bottom-0 left-0 right-0 bg-[#f1f1f1] z-30'>
+	// 			<div className='border-2 h-full mx-auto max-w-prose flex flex-col items-center bg-white space-y-3 uppercase  rounded-lg'>
+	// 				<h1 className=' text-sm text-slate-900 uppercase font-bold md:text-2xl py-3'>
+	// 					{goals} {filterValue} Workout
+	// 				</h1>
+	// 				<div className='w-full h-36 flex flex-col items-center text-slate-900 shadow-lg border border-slate-500/50 mb-3 rounded-lg'>
+	// 					<h1 className='pt-2'>1. {exerciseOne.name}</h1>
+	// 					<div className='flex justify-between w-full h-1/2 py-2 px-8 md:px-20'>
+	// 						<div>
+	// 							<p>reps</p>
+	// 							<p>{repsOne}</p>
+	// 						</div>
+	// 						<div>
+	// 							<p>tempo</p>
+	// 							<p>{tempo}</p>
+	// 						</div>
+	// 						<div>
+	// 							<p>rest</p>
+	// 							<p>{rest}</p>
+	// 						</div>
+	// 					</div>
+	// 					<div className='flex items-center w-full h-full px-3 md:px-10'>
+	// 						<p className='pr-2'>sets</p>
+	// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
+	// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
+	// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
+	// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
+	// 					</div>
+	// 				</div>
+	// 				<div className='w-full h-36 flex flex-col items-center bg-white text-slate-900 shadow-lg border border-slate-500/50 mb-3 rounded-lg'>
+	// 					<h1 className='pt-2'>2. {exerciseTwo.name}</h1>
+	// 					<div className='flex justify-between w-full h-1/2 py-2 px-8 md:px-20'>
+	// 						<div>
+	// 							<p>reps</p>
+	// 							<p>{repsTwo}</p>
+	// 						</div>
+	// 						<div>
+	// 							<p>tempo</p>
+	// 							<p>{tempoTwo}</p>
+	// 						</div>
+	// 						<div>
+	// 							<p>rest</p>
+	// 							<p>{rest}</p>
+	// 						</div>
+	// 					</div>
+	// 					<div className='flex items-center w-full h-full px-3 md:px-10'>
+	// 						<p className='pr-2'>sets</p>
+	// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
+	// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
+	// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
+	// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
+	// 					</div>
+	// 				</div>
+	// 				<div className='w-full h-36 flex flex-col items-center bg-white text-slate-900 shadow-lg border border-slate-500/50 mb-3 rounded-lg'>
+	// 					<h1 className='pt-2'>3. {exerciseThree.name}</h1>
+	// 					<div className='flex justify-between w-full h-1/2 py-2 px-8 md:px-20'>
+	// 						<div>
+	// 							<p>reps</p>
+	// 							<p>{repsThree}</p>
+	// 						</div>
+	// 						<div>
+	// 							<p>tempo</p>
+	// 							<p>{tempo}</p>
+	// 						</div>
+	// 						<div>
+	// 							<p>rest</p>
+	// 							<p>{restTwo}</p>
+	// 						</div>
+	// 					</div>
+	// 					<div className='flex items-center w-full h-full px-3 md:px-10'>
+	// 						<p className='pr-2'>sets</p>
+	// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
+	// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
+	// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
+	// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
+	// 					</div>
+	// 				</div>
+	// 				<div className='w-full h-36 flex flex-col items-center bg-white text-slate-900 shadow-lg border border-slate-500/50 mb-3 rounded-lg'>
+	// 					<h1 className='pt-2'>4. {exerciseFour.name}</h1>
+	// 					<div className='flex justify-between w-full h-1/2 py-2 px-8 md:px-20'>
+	// 						<div>
+	// 							<p>reps</p>
+	// 							<p>{repsFour}</p>
+	// 						</div>
+	// 						<div>
+	// 							<p>tempo</p>
+	// 							<p>{tempoTwo}</p>
+	// 						</div>
+	// 						<div>
+	// 							<p>rest</p>
+	// 							<p>{rest}</p>
+	// 						</div>
+	// 					</div>
+	// 					<div className='flex items-center w-full h-full px-3 md:px-10'>
+	// 						<p className='pr-2'>sets</p>
+	// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
+	// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
+	// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
+	// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
+	// 					</div>
+	// 				</div>
+	// 				<div className='w-full h-36 flex flex-col items-center bg-white text-slate-900 shadow-lg border border-slate-500/50 mb-3 rounded-lg'>
+	// 					<h1 className='pt-2'>5. {exerciseFive.name}</h1>
+	// 					<div className='flex justify-between w-full h-1/2 py-2 px-8 md:px-20'>
+	// 						<div>
+	// 							<p>reps</p>
+	// 							<p>{repsFive}</p>
+	// 						</div>
+	// 						<div>
+	// 							<p>tempo</p>
+	// 							<p>{tempo}</p>
+	// 						</div>
+	// 						<div>
+	// 							<p>rest</p>
+	// 							<p>{rest}</p>
+	// 						</div>
+	// 					</div>
+	// 					<div className='flex items-center w-full h-full px-3 md:px-10'>
+	// 						<p className='pr-2'>sets</p>
+	// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
+	// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
+	// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
+	// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
+	// 					</div>
+	// 				</div>
+	// 				<div className='w-full h-36 flex flex-col items-center bg-white text-slate-900 shadow-lg border border-slate-500/50 mb-3 rounded-lg'>
+	// 					<h1 className='pt-2'>6. {exerciseSix.name}</h1>
+	// 					<div className='flex justify-between w-full h-1/2 py-2 px-8 md:px-20'>
+	// 						<div>
+	// 							<p>reps</p>
+	// 							<p>{repsSix}</p>
+	// 						</div>
+	// 						<div>
+	// 							<p>tempo</p>
+	// 							<p>{tempoTwo}</p>
+	// 						</div>
+	// 						<div>
+	// 							<p>rest</p>
+	// 							<p>{restTwo}</p>
+	// 						</div>
+	// 					</div>
+	// 					<div className='flex items-center w-full h-full px-3 md:px-10'>
+	// 						<p className='pr-2'>sets</p>
+	// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
+	// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
+	// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
+	// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
+	// 					</div>
+	// 				</div>
+	// 			</div>
+	// 		</section>
+	// 	);
+	// }
+
+	// Example not using variables to save API call amount during development
 	return (
 		<section className='absolute h-[120%] top-0 bottom-0 left-0 right-0 bg-[#f1f1f1] z-30'>
 			<div className='border-2 h-full mx-auto max-w-prose flex flex-col items-center bg-white space-y-3 uppercase  rounded-lg'>
@@ -244,162 +401,5 @@ function WorkoutModal({
 		</section>
 	);
 }
-
-// // Example not using variables to save API call amount during development
-// function WorkoutModal() {
-// 	return (
-// 		<section className='absolute h-[120%] top-0 bottom-0 left-0 right-0 bg-[#f1f1f1] z-30'>
-// 			<div className='border-2 h-full mx-auto max-w-prose flex flex-col items-center bg-white space-y-3 uppercase  rounded-lg'>
-// 				<h1 className=' text-sm text-slate-900 uppercase font-bold md:text-2xl py-3'>
-// 					Power and Strength Leg Workout
-// 				</h1>
-// 				<div className='w-full h-36 flex flex-col items-center text-slate-900 shadow-lg border border-slate-500/50 mb-3 rounded-lg'>
-// 					<h1 className='pt-2'>1. Exercise Name Here</h1>
-// 					<div className='flex justify-between w-full h-1/2 py-2 px-8 md:px-20'>
-// 						<reps>
-// 							<p>reps</p>
-// 							<p>6</p>
-// 						</reps>
-// 						<tempo>
-// 							<p>tempo</p>
-// 							<p>1-0-1</p>
-// 						</tempo>
-// 						<rest>
-// 							<p>rest</p>
-// 							<p>120s</p>
-// 						</rest>
-// 					</div>
-// 					<sets className='flex items-center w-full h-full px-3 md:px-10'>
-// 						<p className='pr-2'>sets</p>
-// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
-// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
-// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
-// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
-// 					</sets>
-// 				</div>
-// 				<div className='w-full h-36 flex flex-col items-center bg-white text-slate-900 shadow-lg border border-slate-500/50 mb-3 rounded-lg'>
-// 					<h1 className='pt-2'>2. Exercise Name Here</h1>
-// 					<div className='flex justify-between w-full h-1/2 py-2 px-8 md:px-20'>
-// 						<reps>
-// 							<p>reps</p>
-// 							<p>6</p>
-// 						</reps>
-// 						<tempo>
-// 							<p>tempo</p>
-// 							<p>1-0-1</p>
-// 						</tempo>
-// 						<rest>
-// 							<p>rest</p>
-// 							<p>120s</p>
-// 						</rest>
-// 					</div>
-// 					<sets className='flex items-center w-full h-full px-3 md:px-10'>
-// 						<p className='pr-2'>sets</p>
-// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
-// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
-// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
-// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
-// 					</sets>
-// 				</div>
-// 				<div className='w-full h-36 flex flex-col items-center bg-white text-slate-900 shadow-lg border border-slate-500/50 mb-3 rounded-lg'>
-// 					<h1 className='pt-2'>3. Exercise Name Here</h1>
-// 					<div className='flex justify-between w-full h-1/2 py-2 px-8 md:px-20'>
-// 						<reps>
-// 							<p>reps</p>
-// 							<p>6</p>
-// 						</reps>
-// 						<tempo>
-// 							<p>tempo</p>
-// 							<p>1-0-1</p>
-// 						</tempo>
-// 						<rest>
-// 							<p>rest</p>
-// 							<p>120s</p>
-// 						</rest>
-// 					</div>
-// 					<sets className='flex items-center w-full h-full px-3 md:px-10'>
-// 						<p className='pr-2'>sets</p>
-// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
-// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
-// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
-// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
-// 					</sets>
-// 				</div>
-// 				<div className='w-full h-36 flex flex-col items-center bg-white text-slate-900 shadow-lg border border-slate-500/50 mb-3 rounded-lg'>
-// 					<h1 className='pt-2'>4. Exercise Name Here</h1>
-// 					<div className='flex justify-between w-full h-1/2 py-2 px-8 md:px-20'>
-// 						<reps>
-// 							<p>reps</p>
-// 							<p>6</p>
-// 						</reps>
-// 						<tempo>
-// 							<p>tempo</p>
-// 							<p>1-0-1</p>
-// 						</tempo>
-// 						<rest>
-// 							<p>rest</p>
-// 							<p>120s</p>
-// 						</rest>
-// 					</div>
-// 					<sets className='flex items-center w-full h-full px-3 md:px-10'>
-// 						<p className='pr-2'>sets</p>
-// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
-// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
-// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
-// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
-// 					</sets>
-// 				</div>
-// 				<div className='w-full h-36 flex flex-col items-center bg-white text-slate-900 shadow-lg border border-slate-500/50 mb-3 rounded-lg'>
-// 					<h1 className='pt-2'>5. Exercise Name Here</h1>
-// 					<div className='flex justify-between w-full h-1/2 py-2 px-8 md:px-20'>
-// 						<reps>
-// 							<p>reps</p>
-// 							<p>6</p>
-// 						</reps>
-// 						<tempo>
-// 							<p>tempo</p>
-// 							<p>1-0-1</p>
-// 						</tempo>
-// 						<rest>
-// 							<p>rest</p>
-// 							<p>120s</p>
-// 						</rest>
-// 					</div>
-// 					<sets className='flex items-center w-full h-full px-3 md:px-10'>
-// 						<p className='pr-2'>sets</p>
-// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
-// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
-// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
-// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
-// 					</sets>
-// 				</div>
-// 				<div className='w-full h-36 flex flex-col items-center bg-white text-slate-900 shadow-lg border border-slate-500/50 mb-3 rounded-lg'>
-// 					<h1 className='pt-2'>6. Exercise Name Here</h1>
-// 					<div className='flex justify-between w-full h-1/2 py-2 px-8 md:px-20'>
-// 						<reps>
-// 							<p>reps</p>
-// 							<p>6</p>
-// 						</reps>
-// 						<tempo>
-// 							<p>tempo</p>
-// 							<p>1-0-1</p>
-// 						</tempo>
-// 						<rest>
-// 							<p>rest</p>
-// 							<p>120s</p>
-// 						</rest>
-// 					</div>
-// 					<sets className='flex items-center w-full h-full px-3 md:px-10'>
-// 						<p className='pr-2'>sets</p>
-// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
-// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
-// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
-// 						<span className='h-2 w-[25%] bg-slate-500/50 mr-0.5'></span>
-// 					</sets>
-// 				</div>
-// 			</div>
-// 		</section>
-// 	);
-// }
 
 export default WorkoutModal;
