0100c4b03710903eeefbfeab5c6d02be0dd6666d
Justin Scorzafava <jkscorzafava@gmail.com>
2022-10-14 16:30:52 -0600

    moved user image and email to top of sidebar

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -15,19 +15,9 @@ const Sidebar = ({ isHamburgerMenuOpen, setIsHamburgerMenuOpen, handleClick, use
 					: "absolute top-0 right-0 left-0 bg-slate-800 w-full text-5xl hidden flex-col justify-content-center z-40 origin-top animate-open-menu"
 			}
 		>
-			<ul className='flex flex-col min-h-[120vh] w-full items-center py-8 sidebar-links'>
-				<li className='w-full text-center text-white mt-16 py-6 hover:opacity-90 hover:bg-slate-700'>
-					<Link to='/' onClick={() => setIsHamburgerMenuOpen(false)}>
-						Track Workout
-					</Link>
-				</li>
-				<li className='w-full text-center text-white py-6 hover:opacity-90 hover:bg-slate-700'>
-					<Link to='/generateworkout' onClick={() => setIsHamburgerMenuOpen(false)}>
-						Generate Workout
-					</Link>
-				</li>
+			<ul className='flex flex-col min-h-[120vh] w-full items-center sidebar-links'>
 				{user.user.userpicture === "1" && (
-					<li className='relative group flex py-3 items-center'>
+					<li className='relative group flex py-3 pr-20 items-center'>
 						<img
 							src={userImage1}
 							alt=''
@@ -39,7 +29,7 @@ const Sidebar = ({ isHamburgerMenuOpen, setIsHamburgerMenuOpen, handleClick, use
 					</li>
 				)}
 				{user.user.userpicture === "2" && (
-					<li className='relative group flex py-3 items-center'>
+					<li className='relative group flex py-3 pr-20 items-center'>
 						<img
 							src={userImage2}
 							alt=''
@@ -51,7 +41,7 @@ const Sidebar = ({ isHamburgerMenuOpen, setIsHamburgerMenuOpen, handleClick, use
 					</li>
 				)}
 				{user.user.userpicture === "3" && (
-					<li className='relative group flex py-3 items-center'>
+					<li className='relative group flex py-3 pr-20 items-center'>
 						<img
 							src={userImage3}
 							alt=''
@@ -63,7 +53,7 @@ const Sidebar = ({ isHamburgerMenuOpen, setIsHamburgerMenuOpen, handleClick, use
 					</li>
 				)}
 				{user.user.userpicture === "4" && (
-					<li className='relative group flex py-3 items-center'>
+					<li className='relative group flex py-3 pr-20 items-center'>
 						<img
 							src={userImage4}
 							alt=''
@@ -74,9 +64,19 @@ const Sidebar = ({ isHamburgerMenuOpen, setIsHamburgerMenuOpen, handleClick, use
 						</span>
 					</li>
 				)}
+				<li className='w-full text-center text-3xl text-white mt-6 px-3 py-6 hover:opacity-90 hover:bg-slate-700'>
+					<Link to='/' onClick={() => setIsHamburgerMenuOpen(false)}>
+						Track Workout
+					</Link>
+				</li>
+				<li className='w-full text-center text-3xl text-white px-3 py-6 hover:opacity-90 hover:bg-slate-700'>
+					<Link to='/generateworkout' onClick={() => setIsHamburgerMenuOpen(false)}>
+						Generate Workout
+					</Link>
+				</li>
 				<li>
 					<button
-						className='text-white text-md px-8 py-3 border-2 border-white rounded-lg mt-1 hover:bg-purple-500 hover:text-white hover:bg-opacity-60'
+						className='text-white text-xl mt-4 px-8 py-3 border-2 border-white rounded-lg hover:bg-purple-500 hover:text-white hover:bg-opacity-60'
 						onClick={handleClick}
 					>
 						Log out
