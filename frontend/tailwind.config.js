19d6cc8925733f340d45b4c3396e30ecbe904e6d
Justin Scorzafava <jkscorzafava@gmail.com>
2022-10-13 18:40:20 -0600

    removed animation code inserted into tailwind config previously

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,14 +9,6 @@ module.exports = {
 				offWhiteOne: "#E9DAC1",
 				offWhiteTwo: "#F7ECDE",
 			},
-			keyframes: {
-				"0%": { transform: "scaleY(0)" },
-				"80%": { transform: "scaleY(1.2)" },
-				"100%": { transform: "scaleY(1)" },
-			},
-		},
-		animation: {
-			"open-menu": "open-menu 0.5s ease-in-out forwards",
 		},
 	},
 	plugins: [],
